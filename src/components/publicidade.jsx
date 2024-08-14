6aa28ab98d855955f75e18c52e75cc181c049ae2
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-14 19:00:03 +0100

    fix: src/components/publicidade.jsx. Criação de uma imagem em src/components/publicidade.jsx; Modificação de código em src/components/publicidade.jsx; Alteração de texto em src/components/publicidade.jsx; Criação de uma constante em src/components/publicidade.jsx; Mudança de texto em um elemento HTML em src/components/publicidade.jsx; Alteração de conteúdo em array ou objeto em src/components/publicidade.jsx

--- src/components/publicidade.jsx
+++ src/components/publicidade.jsx
@@ -11,14 +11,15 @@ const Pub = () => {
       <div className="pubb">
         <div className="text-center">
           <b className="text-secondary f-20">Publicidade</b>
-       
-       <br />
-        <span className="f-14 text-secondary">Clique no anuncio</span>
+
+          <br />
+          <span className="f-14 text-secondary">Clique no anuncio</span>
         </div>
-        <a href="https://arotec.ao/pt/loja" target="__blank">
+        <a href="https://www.m2jtecnologia.ao/" target="__blank">
           <div className="publicidad text-white my-3 ">
-            <img src={pub3} alt="" />
-            <img src={pub4} alt="" />
+            <img src={'https://media.licdn.com/dms/image/v2/D4D22AQGzd6c-ZQVdkA/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1713438279526?e=1726704000&v=beta&t=r5ZlLu865nMRyDz7VxV_tvCsB9hlHE315pNONVmJL1c'} alt="" />
+            <img src={'https://media.licdn.com/dms/image/D4D22AQFz1inOMuXSKw/feedshare-shrink_800/0/1705486380366?e=1726704000&v=beta&t=tqYA8ZWsjee1SgEagf_rOuBea1xpuQhWj8rWrfi4MGY'} alt="" />
+            <img src={'https://media.licdn.com/dms/image/v2/D4D22AQFVqa6Cxjawzw/feedshare-shrink_800/feedshare-shrink_800/0/1705315159975?e=1726704000&v=beta&t=X07ockOrqYRQuzVk8e3xUfkUJndIXfxA09mJt_Rb56M'} alt="" />
           </div>
         </a>
       </div>
