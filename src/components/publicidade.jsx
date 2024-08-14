869115d35e09434e2a47f8c0a581632066075725
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-14 19:06:15 +0100

    fix: src/components/publicidade.jsx. Criação de uma imagem em src/components/publicidade.jsx; Modificação de código em src/components/publicidade.jsx; Alteração de texto em src/components/publicidade.jsx; Criação de uma constante em src/components/publicidade.jsx; Mudança de texto em um elemento HTML em src/components/publicidade.jsx; Alteração de conteúdo em array ou objeto em src/components/publicidade.jsx

--- src/components/publicidade.jsx
+++ src/components/publicidade.jsx
@@ -15,7 +15,7 @@ const Pub = () => {
           <br />
           <span className="f-14 text-secondary">Clique no anuncio</span>
         </div>
-        <a href="https://www.m2jtecnologia.ao/" target="__blank">
+        <a href="https://www.linkedin.com/company/m2j-tecnologia/about/" target="__blank">
           <div className="publicidad text-white my-3 ">
             <img src={'https://media.licdn.com/dms/image/v2/D4D22AQGzd6c-ZQVdkA/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1713438279526?e=1726704000&v=beta&t=r5ZlLu865nMRyDz7VxV_tvCsB9hlHE315pNONVmJL1c'} alt="" />
             <img src={'https://media.licdn.com/dms/image/D4D22AQFz1inOMuXSKw/feedshare-shrink_800/0/1705486380366?e=1726704000&v=beta&t=tqYA8ZWsjee1SgEagf_rOuBea1xpuQhWj8rWrfi4MGY'} alt="" />
